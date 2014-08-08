0093f3ba165983245603cd1be58a253ed0611d24
Tom Henderson <tomhenderson@mac.com>
2014-08-08 13:42:59 +1200

    Added styles, images, header and footer.

--- footer.php
+++ footer.php
@@ -1,3 +1,36 @@
+</div>
+<!-- end of content left (main content column) -->
+<!-- sidebar -->
+<div id="sidebar">
+</div>
+<!-- end of sidebar -->
+
+<div class="clearFrame"></div>
+</div>
+<!-- end of content -->
+
+<div class="push"></div>
+</div>
+<!-- end of content wraper -->
+
+</div>
+<!-- end of wrapper -->
+
+<!-- footer -->
+<div id="footer">
+	<p>33 Drake St Freemans Bay Auckland : Tel. 09 379-8167 : Open Monday to Thursday from 4pm, Friday to Sunday from 12pm.</p>
+	<div id="footerlinks">
+	    <?php
+        $defaults = array(
+          'theme_location' => 'primary',
+          'container' => false
+        )
+        wp_nav_menu( $defaults );
+      ?>
+	</div> 
+</div>       
+<!-- end of footer -->
+
 <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -10,9 +10,42 @@
       bloginfo( 'name' );
     ?>
   </title>
+  <meta name="verify-v1" content="Nwq9KZmKC6BRThnw0RrGrWYo9mshW9M6FCkM9hbObaQ=" />
+  <meta name="description" content="La Zeppa - the ultimate meeting place where you decide how you want your night to go. 33 Drake Street Freemans Bay Auckland." />
+  <meta name="keywords" content="La Zeppa, La Zeppa Restaurant, Restaurant, Drake Street Auckland, Auckland, Freemans Bay Street, Tapas, DJ's, Music, Women, Functions, Fine dining, Fine Wine, Industrial,  Cold Tapas, Hot Tapas, Cocktails" />
+  
+  <link href="css/screen.css" rel="stylesheet" type="text/css" media="screen" />
+  <link href="css/print.css" rel="stylesheet" type="text/css" media="print" />
+
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
-<?php wp_nav_menu( array('theme_location' => 'primary') ); ?>
+<!-- wrapper -->
+<div id="wrapper">
+
+  <!-- content wraper -->
+  <div id="content_wrapper">
+  
+    <!-- header -->
+    <div id="header"></div>
+    <!-- end of header -->
+  
+    <!-- navigation menu -->
+    <div id="nav">
+      <?php
+        $defaults = array(
+          'theme_location' => 'primary',
+          'container' => false
+        )
+        wp_nav_menu( $defaults );
+      ?>
+    </div>
+    <!-- end of nav menu -->
+
+    <!-- content -->
+    <div id="content">
+    
+      <!-- content left (main content column) -->
+      <div id="content_left">
