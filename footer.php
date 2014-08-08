8380b4d7ffa857d1daf4e4715b1b2990e4f93992
Tom Henderson <tomhenderson@mac.com>
2014-08-08 13:51:41 +1200

    Add social icons.

--- footer.php
+++ footer.php
@@ -27,6 +27,10 @@
         );
         wp_nav_menu( $defaults );
       ?>
+      <ul>
+      	<li><a href="https://www.facebook.com/pages/La-Zeppa/211822002145"><img src="<?=get_template_directory_uri();?>images/facebook36x36.png" /></a></li>
+	    <li><a href="http://twitter.com/#!/LaZeppaBar"><img src="<?=get_template_directory_uri();?>images/twitter36x36.png"></a></li>
+      </ul>
 	</div> 
 </div>       
 <!-- end of footer -->
