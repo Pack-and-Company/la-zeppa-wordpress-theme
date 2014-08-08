a744738b510d69c02c4c80abb5f3864783d5dced
Tom Henderson <tomhenderson@mac.com>
2014-08-08 15:04:13 +1200

    Move social icons into footer.

--- footer.php
+++ footer.php
@@ -14,14 +14,15 @@
 <div id="footer">
 	<p>33 Drake St Freemans Bay Auckland : Tel. 09 379-8167 : Open Monday to Thursday from 4pm, Friday to Sunday from 12pm.</p>
 	<div id="footerlinks">
+      <ul>
 	    <?php
         $defaults = array(
           'theme_location' => 'primary',
-          'container' => false
+          'container' => false,
+          'items_wrap' => '%3$s'
         );
         wp_nav_menu( $defaults );
       ?>
-      <ul>
       	<li><a href="https://www.facebook.com/pages/La-Zeppa/211822002145"><img src="<?=get_template_directory_uri();?>/images/facebook36x36.png" /></a></li>
 	    <li><a href="http://twitter.com/#!/LaZeppaBar"><img src="<?=get_template_directory_uri();?>/images/twitter36x36.png"></a></li>
       </ul>
