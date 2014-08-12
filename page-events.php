4e90ece0957627848f62d0a1e1f667aafdb7c5f5
Tom Henderson <tomhenderson@mac.com>
2014-08-13 09:59:25 +1200

    Make events a post type.

--- page-events.php
+++ page-events.php
@@ -1,4 +1,17 @@
 <?php
+/*
+Template Name: What's On
+*/
+?>
+
+<?php get_header(); ?>
+
+<!-- content left (main content column) -->
+<div id="content_left">
+	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	  <?php the_content(); ?>
+	<?php endwhile; wp_reset_query(); ?>
+<?php
 
 function xmlToArray($xml,$ns=null){
 	$a = array();
@@ -98,4 +111,19 @@ foreach ($eventsData['event'] as $event) {
 }
 print "</table>\n";
 ?>
+</div>
+<!-- end of content left (main content column) -->
+<!-- sidebar -->
+<div id="sidebar">
+	<?php
+	if ( has_post_thumbnail() ) {
+		the_post_thumbnail();
+		the_post_thumbnail_caption();
+	}
+	?>
+</div>
+<!-- end of sidebar -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
 
